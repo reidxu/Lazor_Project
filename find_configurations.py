fc4bd52606b73bf4814fe2e8329e1ce4b5cf6ec6
LAUREN CONWAY <laurenrconway123@gmail.com>
2023-10-30 14:50:41 -0400

    Find all possible configurations for available positions on grid

---
+++ find_configurations.py
@@ -0,0 +1,56 @@
+'''
+Find all possible configurations of the board
+'''
+from itertools import permutations
+from Read_in_bff_file import get_grid
+from Read_in_bff_file import get_blocks
+def get_permutations(list_for_perm):
+    list_for_perm = list_for_perm
+    config_list = []
+    for p in permutations(list_for_perm):
+        if p not in config_list:
+            config_list.append(p)
+        else:
+            continue
+    return config_list
+
+
+def get_config(grid, blocks):
+    available = 0
+    for x in grid:
+        for y in x:
+            if y == 'o':
+                available = available + 1
+
+    block_types = []
+    block_numbers = []
+    for i in blocks:
+        block_types.append(i)
+        block_numbers.append(blocks[i])
+
+    blocks_sum = 0
+    for num in block_numbers:
+        blocks_sum = blocks_sum + num
+
+    open_spots = available - blocks_sum
+    list_for_perm = []
+    for i in range(open_spots):
+        list_for_perm.append('o')
+    for j in block_types:
+        if j == 'reflective_blocks':
+            for k in range(blocks[j]):
+                list_for_perm.append('A')
+        if j == 'opaque_blocks':
+            for l in range(blocks[j]):
+                list_for_perm.append('B')
+        if j == 'refractive_blocks':
+            for h in range(blocks[j]):
+                list_for_perm.append('C')
+
+    return get_permutations(list_for_perm)
+
+
+if __name__ == "__main__":
+    grid = get_grid('tiny_5.bff')
+    blocks = get_blocks('tiny_5.bff')
+    config = get_config(grid, blocks)

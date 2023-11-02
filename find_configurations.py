3cd90ddfa3b1fede20e70c8368b4b4b43c8d0a92
Lconway4C <115810068+Lconway4C@users.noreply.github.com>
2023-11-02 15:14:07 -0400

    Update find_configurations.py
    Fix time problem

--- find_configurations.py
+++ find_configurations.py
@@ -1,21 +1,51 @@
 '''
 Find all possible configurations of the board
 '''
-from itertools import permutations
+from more_itertools import distinct_permutations as idp
 from Read_in_bff_file import get_grid
 from Read_in_bff_file import get_blocks
 
 def get_permutations(list_for_perm):
+    '''
+    Find all the permutations of the avaliable spots on the board
+
+    **Parameters**
+
+        list_for_perm: *list*
+            The list of options for open spots
+
+    **Returns**
+
+        config_list: *list*
+            A list containing all lists of permutations of the availabe spots.
+    '''  
     config_list = []
-    for p in permutations(list_for_perm):
-        if p not in config_list:
-            config_list.append(p)
-        else:
-            continue
+    perm = idp(list_for_perm)
+    config_list = list(perm)
     return config_list
 
 
-def get_config(grid, blocks):
+def get_configs(grid, blocks):
+    '''
+    Find all the configurations of the available spots on the board
+    * elements are aliased as integers to save time
+    * 0 = empty spot ('o')
+    * 1 = reflective block ('A')
+    * 2 = opaque block ('B')
+    * 3 = refractive block ('C')
+
+    **Parameters**
+
+        grid: *array*
+            The grid of board elements
+        blocks: *dict*
+            A dictionary containing the types and number of blocks
+
+    **Returns**
+
+        config_list: *list*
+            A list containing all lists of configurations of the availabe spots.
+    '''  
     available = 0
     for x in grid:
         for y in x:
@@ -35,22 +65,39 @@ def get_config(grid, blocks):
     open_spots = available - blocks_sum
     list_for_perm = []
     for i in range(open_spots):
-        list_for_perm.append('o')
+        list_for_perm.append(int(0))
+
     for j in block_types:
         if j == 'reflective_blocks':
             for k in range(blocks[j]):
-                list_for_perm.append('A')
+                list_for_perm.append(int(1))
         if j == 'opaque_blocks':
             for l in range(blocks[j]):
-                list_for_perm.append('B')
+                list_for_perm.append(int(2))
         if j == 'refractive_blocks':
             for h in range(blocks[j]):
-                list_for_perm.append('C')
+                list_for_perm.append(int(3))
 
-    return get_permutations(list_for_perm)
+    configs = get_permutations(list_for_perm)
+    return configs
 
 
 def get_boards(grid, configs):
+    '''
+    Find all the configurations of the available spots on the board
+
+    **Parameters**
+
+        grid: *array*
+            The grid of board elements
+        configs: *list*
+            A list containing all lists of configurations of the availabe spots.
+
+    **Returns**
+
+        boards: *list*
+            A list containing all lists of configurations of the board.
+    '''  
     list_of_board = []
     boards = []
 
